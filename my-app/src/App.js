4b2b0c10a364f9923073727dfd2e9678ab26a692
Merbock <merbock7@gmail.com>
2025-01-24 00:01:47 +0300

    app withouth jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,28 +3,39 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	const currentYear = new Date().getFullYear(); //Императивный стиль: здесь мы используем встроенный метод Date для получения текущего года.
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p> {/*Декларативный стиль: здесь мы просто указываем, что хотим отобразить текущий год.*/}
-			</header>
-		</div>
+	const currentYear = new Date().getFullYear();
+
+	return React.createElement(
+	  'div',
+	  { className: 'App' },
+	  React.createElement(
+	  'header',
+	  { className: 'App-header' },
+	  React.createElement('img', {
+		src: logo,
+		className: 'App-logo',
+		alt: 'logo',
+	  }),
+	  React.createElement(
+		'p',
+		null,
+		'Edit ',
+		React.createElement('code', null, 'src/App.js'),
+		' and save to reload.'
+	  ),
+	  React.createElement(
+		'a',
+		{
+		className: 'App-link',
+		href: 'https://reactjs.org',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+		},
+		'Learn React'
+	  ),
+	  React.createElement('p', null, currentYear)
+	  )
 	);
-}
-
-
+	}
 
 export default App;
